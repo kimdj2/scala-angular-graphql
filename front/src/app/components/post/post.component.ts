78ba062add1fed34b674d81ac2abc2e045ac46d6
KimDongjin <1222kdj@gmail.com>
2022-08-28 14:51:04 +0900

    commit

--- front/src/app/components/post/post.component.ts
+++ front/src/app/components/post/post.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { find, map, Observable } from 'rxjs';
 import { Post } from 'src/app/models/post';
 import { PostService } from 'src/app/services/post/post.service';
 
@@ -11,7 +11,7 @@ import { PostService } from 'src/app/services/post/post.service';
 })
 export class PostComponent implements OnInit {
 
-  posts!: Observable<Post[]>;
+  posts: Observable<Post[]>;
   postForm!: FormGroup;
 
   constructor(
@@ -24,6 +24,12 @@ export class PostComponent implements OnInit {
       content: new FormControl(''),
     })
     this.posts = this.postService.getAllPosts();
+  }
+
+  test(id: number): Observable<Post> {
+    return this.posts.pipe(
+      map(v => v.find(v => v.id === id) as Post)
+    )
   }
 
   onAdd(): void {
