ca74b3d082649f9b62c57a4601ca3fbce68bc566
KimDongjin <1222kdj@gmail.com>
2022-08-28 14:53:00 +0900

    angular14

--- front/src/app/components/post/post.component.ts
+++ front/src/app/components/post/post.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import { find, map, Observable } from 'rxjs';
 import { Post } from 'src/app/models/post';
 import { PostService } from 'src/app/services/post/post.service';
@@ -12,16 +12,16 @@ import { PostService } from 'src/app/services/post/post.service';
 export class PostComponent implements OnInit {
 
   posts: Observable<Post[]>;
-  postForm!: FormGroup;
+  postForm!: UntypedFormGroup;
 
   constructor(
     private postService: PostService
   ) { }
 
   ngOnInit(): void {
-    this.postForm = new FormGroup({
-      title: new FormControl(''),
-      content: new FormControl(''),
+    this.postForm = new UntypedFormGroup({
+      title: new UntypedFormControl(''),
+      content: new UntypedFormControl(''),
     })
     this.posts = this.postService.getAllPosts();
   }
